e43d68bd935f988c2de00163c0b3c58774781a09
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2020-11-15 22:12:40 -0300

    feat: form callback na nova lista

--- src/pages/list/new.jsx
+++ src/pages/list/new.jsx
@@ -1,8 +1,10 @@
 import ListForm from 'containers/ListForm';
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { withAppStore } from 'store';
 
 const NewList = props => {
+  const router = useRouter();
   const [initialValues, setInitialValues] = useState({
     name: '',
     description: '',
@@ -24,7 +26,17 @@ const NewList = props => {
     }
   }, [props.appStore?.state?.userId]);
 
-  return <ListForm op="C" initialValues={initialValues} />;
+  const onListCreated = data => {
+    props.appStore.actions.setList(data, router.push(`/list/${data.id}`));
+  };
+
+  return (
+    <ListForm
+      op="C"
+      initialValues={initialValues}
+      successCallback={onListCreated}
+    />
+  );
 };
 
 export default withAppStore(NewList);
